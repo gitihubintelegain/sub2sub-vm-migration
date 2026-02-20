5bd9b85dee0fde05f3bdb625398c049ec6fc13d2
gitihubintelegain <githubintelegain@gmail.com>
2026-02-20 16:07:11 +0530

    Update 03-prepare-for-move.ps1

--- scripts/phases/03-prepare-for-move.ps1
+++ scripts/phases/03-prepare-for-move.ps1
@@ -13,40 +13,58 @@ Write-Host "========================================="
 Write-Host "Phase 3 - Prepare VM For Move"
 Write-Host "========================================="
 
-Set-AzContext -SubscriptionId $SourceSubscription -ErrorAction Stop
+Set-AzContext -SubscriptionId $SourceSubscription -ErrorAction Stop | Out-Null
 
 $vm = Get-AzVM -Name $VMName -ResourceGroupName $ResourceGroup -ErrorAction Stop
 
-# Deallocate
-$status = Get-AzVM -Name $VMName -ResourceGroupName $ResourceGroup -Status
+# -------------------------------------------------------
+# Deallocate VM
+# -------------------------------------------------------
+$status = (Get-AzVM -Name $VMName -ResourceGroupName $ResourceGroup -Status)
+
 if ($status.PowerState -ne "VM deallocated") {
     Write-Host "Deallocating VM..."
-    Stop-AzVM -Name $VMName -ResourceGroupName $ResourceGroup -Force
+    Stop-AzVM -Name $VMName -ResourceGroupName $ResourceGroup -Force | Out-Null
     Write-Host "VM deallocated."
 }
 
+# -------------------------------------------------------
 # NIC
+# -------------------------------------------------------
 $nicId = $vm.NetworkProfile.NetworkInterfaces[0].Id
 $nicName = ($nicId -split "/")[-1]
 $nic = Get-AzNetworkInterface -Name $nicName -ResourceGroupName $ResourceGroup
 
+# -------------------------------------------------------
 # Detach Public IP
+# -------------------------------------------------------
 if ($nic.IpConfigurations[0].PublicIpAddress) {
     Write-Host "Detaching Public IP..."
     $nic.IpConfigurations[0].PublicIpAddress = $null
-    Set-AzNetworkInterface -NetworkInterface $nic
+    Set-AzNetworkInterface -NetworkInterface $nic | Out-Null
     Write-Host "Public IP detached."
 }
 
-# Collect dependencies
+# -------------------------------------------------------
+# Collect Dependencies
+# -------------------------------------------------------
+
 $resourcesToMove = @()
 
+# VM
 $resourcesToMove += $vm.Id
+
+# NIC
 $resourcesToMove += $nic.Id
+
+# OS Disk
 $resourcesToMove += $vm.StorageProfile.OsDisk.ManagedDisk.Id
 
+# Data Disks
 foreach ($disk in $vm.StorageProfile.DataDisks) {
-    $resourcesToMove += $disk.ManagedDisk.Id
+    if ($disk.ManagedDisk) {
+        $resourcesToMove += $disk.ManagedDisk.Id
+    }
 }
 
 # VNet
@@ -58,8 +76,10 @@ if ($nic.NetworkSecurityGroup) {
     $resourcesToMove += $nic.NetworkSecurityGroup.Id
 }
 
+# Remove duplicates just in case
+$resourcesToMove = $resourcesToMove | Select-Object -Unique
+
 Write-Host "Resources prepared for move:"
 $resourcesToMove | ForEach-Object { Write-Host $_ }
 
-# RETURN ARRAY
 return $resourcesToMove
