bd8f8e0730bd6a63c55fae1fd5031a7737a6fbbc
gitihubintelegain <githubintelegain@gmail.com>
2026-02-20 14:19:45 +0530

    Update main.ps1

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -31,3 +31,18 @@ Write-Host "Phase 2 - Backup Cleanup"
     -ResourceGroup $ResourceGroup `
     -VMName $VMName `
     -SourceVaultName $SourceVaultName
+
+Write-Host "Phase 3 - Prepare For Move"
+
+$resourcesToMove = . "$PSScriptRoot/phases/03-prepare-for-move.ps1" `
+    -SourceSubscription $SourceSubscription `
+    -ResourceGroup $ResourceGroup `
+    -VMName $VMName
+
+Write-Host "Phase 4 - Move Resources"
+
+. "$PSScriptRoot/phases/04-move-resources.ps1" `
+    -SourceSubscription $SourceSubscription `
+    -DestinationSubscription $DestinationSubscription `
+    -ResourceGroup $ResourceGroup `
+    -ResourcesToMove $resourcesToMove
