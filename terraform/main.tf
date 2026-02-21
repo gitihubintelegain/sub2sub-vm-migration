0372f91e5ba5b85294896d4e1c5ea11e991b32ab
gitihubintelegain <githubintelegain@gmail.com>
2026-02-21 15:57:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.95.0"
+      version = ">= 4.0.0"
     }
   }
 }
@@ -14,21 +14,21 @@ provider "azurerm" {
 }
 
 # -----------------------------
-# Backup Vault (Modern Architecture)
+# Backup Vault (Data Protection)
 # -----------------------------
 resource "azurerm_data_protection_backup_vault" "vault" {
   name                = "backup-vault-${var.unique_suffix}"
-  location            = var.location
   resource_group_name = var.resource_group
+  location            = var.location
 
   datastore_type = "VaultStore"
   redundancy     = "LocallyRedundant"
 }
 
 # -----------------------------
-# Backup Policy for Azure VM
+# Backup Policy (VM)
 # -----------------------------
-resource "azurerm_data_protection_backup_policy_azure_vm" "policy" {
+resource "azurerm_data_protection_backup_policy" "policy" {
   name     = "vm-daily-policy"
   vault_id = azurerm_data_protection_backup_vault.vault.id
 
@@ -36,24 +36,10 @@ resource "azurerm_data_protection_backup_policy_azure_vm" "policy" {
     "R/2024-01-01T06:00:00+00:00/P1D"
   ]
 
-  retention_rule {
-    name     = "DailyRetention"
-    priority = 1
-
-    criteria {
-      absolute_criteria = "FirstOfDay"
-    }
-
-    life_cycle {
-      duration        = "P7D"
-      data_store_type = "VaultStore"
-    }
-  }
-
   default_retention_rule {
     life_cycle {
-      duration        = "P7D"
       data_store_type = "VaultStore"
+      duration        = "P7D"
     }
   }
 }
@@ -61,10 +47,9 @@ resource "azurerm_data_protection_backup_policy_azure_vm" "policy" {
 # -----------------------------
 # Backup Instance (Protect VM)
 # -----------------------------
-resource "azurerm_data_protection_backup_instance_azure_vm" "vm_backup" {
+resource "azurerm_data_protection_backup_instance" "vm_backup" {
   name               = "vm-backup-${var.unique_suffix}"
   vault_id           = azurerm_data_protection_backup_vault.vault.id
-  source_resource_id = var.vm_id
-
-  backup_policy_id = azurerm_data_protection_backup_policy_azure_vm.policy.id
+  data_source_id     = var.vm_id
+  backup_policy_id   = azurerm_data_protection_backup_policy.policy.id
 }
