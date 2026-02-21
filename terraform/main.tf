d86173c78bb4680ec73e374ce04df2c005e6eef6
gitihubintelegain <githubintelegain@gmail.com>
2026-02-21 16:04:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,6 @@ provider "azurerm" {
   features {}
 }
 
-# -----------------------------
-# Backup Vault (Data Protection)
-# -----------------------------
 resource "azurerm_data_protection_backup_vault" "vault" {
   name                = "backup-vault-${var.unique_suffix}"
   resource_group_name = var.resource_group
@@ -23,33 +20,4 @@ resource "azurerm_data_protection_backup_vault" "vault" {
 
   datastore_type = "VaultStore"
   redundancy     = "LocallyRedundant"
-}
-
-# -----------------------------
-# Backup Policy (VM)
-# -----------------------------
-resource "azurerm_data_protection_backup_policy" "policy" {
-  name     = "vm-daily-policy"
-  vault_id = azurerm_data_protection_backup_vault.vault.id
-
-  backup_repeating_time_intervals = [
-    "R/2024-01-01T06:00:00+00:00/P1D"
-  ]
-
-  default_retention_rule {
-    life_cycle {
-      data_store_type = "VaultStore"
-      duration        = "P7D"
-    }
-  }
-}
-
-# -----------------------------
-# Backup Instance (Protect VM)
-# -----------------------------
-resource "azurerm_data_protection_backup_instance" "vm_backup" {
-  name               = "vm-backup-${var.unique_suffix}"
-  vault_id           = azurerm_data_protection_backup_vault.vault.id
-  data_source_id     = var.vm_id
-  backup_policy_id   = azurerm_data_protection_backup_policy.policy.id
 }
