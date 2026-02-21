84cd0e65b344e2724064991a56184b0cc43588d4
gitihubintelegain <githubintelegain@gmail.com>
2026-02-21 16:24:01 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "vault_name" {
-  value = azurerm_data_protection_backup_vault.vault.name
+  value = azurerm_recovery_services_vault.vault.name
+}
+
+output "policy_name" {
+  value = azurerm_backup_policy_vm.policy.name
 }
