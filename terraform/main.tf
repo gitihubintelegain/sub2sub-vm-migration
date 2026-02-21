f924130e6ac2876bd8d0ff7c0039eae28a6f6d8a
gitihubintelegain <githubintelegain@gmail.com>
2026-02-21 16:24:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,18 @@ provider "azurerm" {
 }
 
 # -----------------------------
+# Safe Naming
+# -----------------------------
+locals {
+  trimmed_vm_name = substr(var.vm_name, 0, 20)
+  vault_name      = "${local.trimmed_vm_name}-rsv-${var.unique_suffix}"
+}
+
+# -----------------------------
 # Recovery Services Vault (LRS)
 # -----------------------------
 resource "azurerm_recovery_services_vault" "vault" {
-  name                = "rsv-${var.unique_suffix}"
+  name                = local.vault_name
   location            = var.location
   resource_group_name = var.resource_group
   sku                 = "Standard"
@@ -30,11 +38,11 @@ resource "azurerm_recovery_services_vault" "vault" {
 # Enhanced Backup Policy (V2)
 # -----------------------------
 resource "azurerm_backup_policy_vm" "policy" {
-  name                = "daily-11am-7days-policy"
+  name                = "${local.trimmed_vm_name}-daily-11am-policy"
   resource_group_name = var.resource_group
   recovery_vault_name = azurerm_recovery_services_vault.vault.name
 
-  policy_type = "V2"   # Enhanced policy
+  policy_type = "V2"
 
   timezone = "India Standard Time"
 
