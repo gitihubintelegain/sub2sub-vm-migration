6e4caf7e5e826f1d11ce4564b8e97fc46075ceeb
gitihubintelegain <githubintelegain@gmail.com>
2026-02-21 16:22:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,39 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_data_protection_backup_vault" "vault" {
-  name                = "backup-vault-${var.unique_suffix}"
-  resource_group_name = var.resource_group
+# -----------------------------
+# Recovery Services Vault (LRS)
+# -----------------------------
+resource "azurerm_recovery_services_vault" "vault" {
+  name                = "rsv-${var.unique_suffix}"
   location            = var.location
+  resource_group_name = var.resource_group
+  sku                 = "Standard"
 
-  datastore_type = "VaultStore"
-  redundancy     = "LocallyRedundant"
+  storage_mode_type   = "LocallyRedundant"
+  soft_delete_enabled = true
+}
+
+# -----------------------------
+# Enhanced Backup Policy (V2)
+# -----------------------------
+resource "azurerm_backup_policy_vm" "policy" {
+  name                = "daily-11am-7days-policy"
+  resource_group_name = var.resource_group
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+
+  policy_type = "V2"   # Enhanced policy
+
+  timezone = "India Standard Time"
+
+  backup {
+    frequency = "Daily"
+    time      = "11:00"
+  }
+
+  retention_daily {
+    count = 7
+  }
+
+  instant_restore_retention_days = 5
 }
